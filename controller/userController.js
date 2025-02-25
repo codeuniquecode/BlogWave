3eb4a8ca38b1b632e35f2dd928aefd29a51a45d8
unique <uniquestha619@gmail.com>
2025-02-25 20:16:12 +0545

    starting project

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,11 @@
 exports.home = (req,res)=>{
     res.render('home');
+    return res.status(200).json({message: "home page here" });
+}
+exports.registerUser = async (req,res)=>{
+    // name,email,passowrd
+    
+}
+exports.loginUser = async (req,res)=>{
+
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const userController = require('../controller/userController');
 
 
-router.get('/',userController.home);
+// router.get('/',userController.home);
+// router.get('/register',userController.registerUser);
+// router.get('/login')
 
+router.route('/').get(userController.home);
 module.exports = router;
