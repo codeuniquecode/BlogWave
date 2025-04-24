8237ae12835c86c5b095ed7f3c5b939093bf143a
Unique Shrestha <uniquestha619@gmail.com>
2025-04-24 20:59:19 +0545

    change db string

---
+++ config/envConfig.js
@@ -0,0 +1,4 @@
+require('dotenv').config();
+exports.envConfig = {
+    url : process.env.mongodbURI
+}
--- model/index.js
+++ model/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const { envConfig } = require('../config/envConfig');
 
-mongoose.connect(process.env.mongodbURI).then(()=>{
+mongoose.connect(envConfig.url).then(()=>{
     console.log("database connected");
 }).catch((e)=>{
     console.log('Error in connecting database'+e);
