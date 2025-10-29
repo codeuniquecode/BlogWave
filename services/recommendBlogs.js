77b496a68821a4ac621148808b0ed8a8f161a743
Unique Shrestha <uniquestha619@gmail.com>
2025-10-29 09:23:44 +0545

    adding google tag for ga4

--- services/recommendBlogs.js
+++ services/recommendBlogs.js
@@ -1,42 +1,65 @@
 const blog = require('../model/blogSchema');
 
-const mongoose = require('mongoose');
-
 async function recommendBlogs(blogId) {
-const blogObjectId = new mongoose.Types.ObjectId(blogId);
-    // Find the blog by ID to get its category
-    const blogData = await blog.findOne({ _id: blogId });
+    const currentBlog = await blog.findById(blogId);
+    if (!currentBlog) return { relatedBlogs: [], sameAuthorBlogs: [] };
 
-    if (!blogData || !blogData.category) {
-        console.log('No blog found with this ID');
-        return [];
+    const otherBlogs = await blog.find({
+        _id: { $ne: blogId },
+        category: currentBlog.category
+    }, 'title description category');
+
+    //calculating the frequency
+    function getWordFreq(text) {
+        const words = text.toLowerCase().split(/\W+/).filter(Boolean);
+        const freq = {};
+        words.forEach(w => freq[w] = (freq[w] || 0) + 1);
+        return freq;
+    }
+//finding similarity
+    function cosineSimilarity(freqA, freqB) {
+        let dot = 0, magA = 0, magB = 0;
+        const wordsA = Object.keys(freqA);
+        const wordsB = Object.keys(freqB);
+        const combinedWords = [...new Set([...wordsA, ...wordsB])];
+
+        combinedWords.forEach(word => {
+            const a = freqA[word] || 0;
+            const b = freqB[word] || 0;
+            dot += a * b;
+            magA += a * a;
+            magB += b * b;
+        });
+
+        return magA && magB ? dot / (Math.sqrt(magA) * Math.sqrt(magB)) : 0;
     }
 
-    console.log('Matching category:', blogData.category);
-    // const relatedBlogs = await blog.find({
-    //     _id: { $ne: blogId }, // Exclude the current blog
-    //     category: blogData.category // Match category
-    // }).limit(3);
-    const relatedBlogs = await blog.aggregate([
-        {
-            $match: {
-                _id: { $ne: blogObjectId }, 
-                category: blogData.category 
-            }
-        },
-        {
-            $sample: { size: 3 } 
-        }
-    ]);
+    // Weighting: title & category appear twice
+    const currentFreq = getWordFreq(
+        `${currentBlog.title} ${currentBlog.title} ${currentBlog.category} ${currentBlog.description}`
+    );
+
+    const relatedBlogs = otherBlogs.map(b => {
+        const freq = getWordFreq(
+            `${b.title} ${b.category} ${b.description}`
+        );
+        return {
+            _id: b._id,
+            title: b.title,
+            category: b.category,
+            score: cosineSimilarity(currentFreq, freq)
+        };
+    })
+    .filter(b => b.score >= 0.2) // ignore unrelated
+    .sort((a, b) => b.score - a.score)
+    .slice(0, 3);
+
     const sameAuthorBlogs = await blog.find({
         _id: { $ne: blogId },
-        author: blogData.author
-      }).limit(3);
-      
+        author: currentBlog.author
+    }).limit(3);
 
-    return {relatedBlogs,sameAuthorBlogs};
+    return { relatedBlogs, sameAuthorBlogs };
 }
-
-
 
 module.exports = recommendBlogs;
